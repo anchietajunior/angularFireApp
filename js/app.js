bf826e142a9a455eafdd4b09374d37dcda3e953d
anchietajunior <santosjr87@gmail.com>
2016-07-25 16:06:58 -0300

    Added bootstrap table and form to create

--- js/app.js
+++ js/app.js
@@ -5,10 +5,33 @@ app.config(function($routeProvider) {
     	controller: 'MyCtrl as ctrl',
         templateUrl : "views/myctrl.html"
     });
-})
+});
+
+app.factory("Customer", ["$firebaseObject",
+  function($firebaseObject) {
+    return function(username) {
+      // create a reference to the database node where we will store our data
+      var randomRoomId = Math.round(Math.random() * 100000000);
+      var ref = new Firebase("https://docs-sandbox.firebaseio.com/af/obj/full/" + randomRoomId);
+      var profileRef = ref.child(username);
+
+      // return it as a synchronized object
+      return $firebaseObject(profileRef);
+    }
+  }
+]);
+
 app.controller("MyCtrl", function($scope, $firebaseArray) {
   var ref = firebase.database().ref().child("customers");
   // create a synchronized array
   // click on `index.html` above to see it used in the DOM!
   $scope.customers = $firebaseArray(ref);
+
+  $scope.saveCustomer = function() {
+      $scope.customer.$save().then(function() {
+        alert('Customer saved!');
+      }).catch(function(error) {
+        alert('Error!');
+      });
+    };
 });
